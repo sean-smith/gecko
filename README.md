8f96a1d5185724de41a074eff8b39ec710a442f8
Sean Smith <seanwssmith@gmail.com>
2017-03-20 15:35:26 -0400

    added nunjuck templating

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # gecko
 Tracking the environmental impact of Uber 🦎
 
-# Install
+## Install
 
 	cd src
 	npm install
 
 You should now have the latest versions of all required packages. Note these are by default installed locally in `node_modules` but ignored by `.gitignore`.
 
-# Run
+## Run
 
 	node app.js
 
--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 var express = require('express')
+var nunjucks = require('nunjucks')
 var app = express()
+
+// Configure Templating Engine Nunjucks
+nunjucks.configure('pages', {
+    autoescape: true,
+    express: app
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.get('/home', function(req, res) {
+	res.render('index.njk')
 })
 
 app.listen(3000, function () {
