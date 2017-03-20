cd13abf21c34509cef7abe7feec6337ff25a2222
GylianeW <gyl.weisenfeld@gmail.com>
2017-03-20 16:06:06 -0400

    added api requests

--- src/app.js
+++ src/app.js
@@ -8,14 +8,25 @@ nunjucks.configure('pages', {
     express: app
 })
 
+var info;
+var request = require('request');
+request({url: 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/year', json: true}, function (error, res, json) {
+    if (!error) {
+      info = json;
+    }
+})
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send(info.menuItem);
 })
 
 app.get('/home', function(req, res) {
-	res.render('index.njk')
+	res.render('index.njk');
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
+
+
+
