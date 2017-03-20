0e4cfbc3a65e9e48818bfe8ac31b615de6b86a07
Ryan Lader <rjlader@gmail.com>
2017-03-20 16:39:32 -0400

    Added functionality for making API Requests, functionizing next

--- src/app.js
+++ src/app.js
@@ -8,17 +8,49 @@ nunjucks.configure('pages', {
     express: app
 })
 
-var info;
+var info; 
+var infoMPG;
 var request = require('request');
-request({url: 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/year', json: true}, function (error, res, json) {
+
+var year = '2012';
+var make = 'toyota';
+var model = 'corolla';
+var requestsSatisfied = 0;
+var urlString = 'http://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=' + year + '&make=' + make + '&model=' + model;
+
+request({url: urlString, json: true}, function (error, res, json) {
     if (!error) {
       info = json;
+	  requestsSatisfied++;
     }
+	else{
+		console.log('threw error');
+	}
 })
 
+console.log(info);
+
+if(requestsSatisfied > 0){
+	var vehicleId = info.menuItem[0].value;
+
+	var urlStringMPG = 'http://www.fueleconomy.gov/ws/rest/ympg/shared/ympgVehicle/' + vehicleID;
+
+	request({url: urlStringMPG, json: true}, function (error, res, json) {
+    	if (!error) {
+    	  infoMPG = json;
+    	}
+	})
+
+	console.log(infoMPG);
+}
+
 app.get('/', function (req, res) {
-  res.send(info.menuItem);
+  res.send(info.menuItem[0]); //Only using first item for now. Might be too much info for auto/manual.
+  if(requestsSatisfied >0){
+	  res.send(infoMPG);
+  }
 })
+
 
 app.get('/home', function(req, res) {
 	res.render('index.njk');
