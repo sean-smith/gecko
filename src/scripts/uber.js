8423eab3f2f98459e1a620e2dccfeb2df734b898
Ryan Lader <rjlader@gmail.com>
2017-04-26 18:15:27 -0400

    Stuff

--- src/scripts/uber.js
+++ src/scripts/uber.js
@@ -8,32 +8,32 @@ function cost_calculator(service, start_time, end_time, miles){
 	var cost_p_min = 0.15
 	var cost_p_mile = 0.90
 	var booking_fee = 0
-	if (service == "UberX"){
+	if (service == "uberX"){
 		min_fare = 5.15
 		booking_fee = 1.65
 	}
-	else if (service == "UberXL"){
+	else if (service == "uberXL"){
 		base_fare = 1.0
 		min_fare = 7.65
 		cost_p_min = 0.30
 		cost_p_mile = 1.55
 		booking_fee = 1.65
 	}
-	else if (service == "UberSelect"){
+	else if (service == "uberSelect"){
 		base_fare = 5.0
 		min_fare = 10.65
 		cost_p_min = 0.40
 		cost_p_mile = 2.35
 		booking_fee = 1.65
 	}
-	else if (service == "UberBlack"){
+	else if (service == "uberBlack"){
 		base_fare = 8.0
 		min_fare = 15.0
 		cost_p_min = 0.45
 		cost_p_mile = 3.55
 		booking_fee = 0.0
 	}
-	else if (service == "UberSUV"){
+	else if (service == "uberSUV"){
 		base_fare = 15.0
 		min_fare = 25.0
 		cost_p_min = 0.55
@@ -44,8 +44,8 @@ function cost_calculator(service, start_time, end_time, miles){
 		return "Unknown"
 	}
 
-	var cost =  (base_fare + (cost_p_min * end_time-start_time / 60.0) + (cost_p_mile * miles) + booking_fee)
-
+	var cost = (base_fare + (cost_p_min * (end_time-start_time) / 60.0) + (cost_p_mile * miles) + booking_fee)
+	console.log(cost)
 	if (cost<min_fare){
 		return min_fare
 	}
@@ -69,7 +69,7 @@ function getRideDetails(req, next) {
 		dist += ride.distance
 		getProductsDescription(req, ride, function(description) {
 			req.user.trips.history[index].description = description
-			var cost = cost_calculator(service, ride.start_time, ride.end_time, ride.distance)
+			var cost = cost_calculator(description.display_name, ride.start_time, ride.end_time, ride.distance)
 			req.user.trips.history[index].cost = cost
 			done[index] = true
 			var terminate = true
@@ -147,13 +147,14 @@ function getRideData(req, res, next) {
 	// Look for info in DB
 	User.findById(req.user.id, function (err, user) {
 
+/*
 		// If it's there return it
 		if (user.trips && user.total_distance) {
 			req.user.trips = user.trips
 			req.user.total_distance = user.total_distance
 			return next(req, res)
 		}
-
+*/
 		// Otherwise fetch it from API
 		getRidesAPI(req, function(req) {
 
